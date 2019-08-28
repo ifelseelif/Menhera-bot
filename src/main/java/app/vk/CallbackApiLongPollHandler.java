6a7ec6f9c033f14c36ca52204fdc4da58d3776c3
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-28 21:38:27 +0900

    added Callback LongPoll Api

---
+++ src/main/java/app/vk/CallbackApiLongPollHandler.java
@@ -0,0 +1,30 @@
+package app.vk;
+
+import app.handler.AppHandler;
+import com.vk.api.sdk.callback.longpoll.CallbackApiLongPoll;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.GroupActor;
+import com.vk.api.sdk.objects.messages.Message;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class CallbackApiLongPollHandler extends CallbackApiLongPoll {
+    private AppHandler handler;
+
+    public CallbackApiLongPollHandler(VkApiClient client, GroupActor actor, VkApp app) {
+        super(client, actor);
+        this.handler = app.getAppHandler();
+    }
+
+    @Override
+    public void messageNew(Integer groupId, Message message) {
+        System.out.println(message);
+        //handler.receive(new model.Message(message.getText(), "" + message.getFromId(), "" + message.getPeerId(), MessageSourceType.VK));
+    }
+
+    @Override
+    public void messageNew(Integer groupId, String secret, Message message) {
+        System.out.println(message);
+    }
+}
